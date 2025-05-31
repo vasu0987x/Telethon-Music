a43385e56b6672a8da0212f24ac22914bcc41a30
vasu0987x <vasupareek2x4@gmail.com>
2025-05-31 10:41:36 +0530

    Update __init__.py

--- Zaid/__init__.py
+++ Zaid/__init__.py
@@ -1,18 +1,14 @@
 import os
-
-from telethon import TelegramClient
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
 import logging
 from pytgcalls import PyTgCalls
-from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
 
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-                    level=logging.INFO)
-
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.INFO)
 
 from Config import Config
+
 BOT_USERNAME = Config.BOT_USERNAME
 ASSISTANT_ID = Config.ASSISTANT_ID
 
@@ -20,5 +16,10 @@ bot = TelegramClient('Zaid', api_id=Config.API_ID, api_hash=Config.API_HASH)
 Zaid = bot.start(bot_token=Config.BOT_TOKEN)
 client = TelegramClient(StringSession(Config.STRING_SESSION), Config.API_ID, Config.API_HASH)
 call_py = PyTgCalls(client)
-client.start()
-call_py.start()
+
+try:
+    client.start()
+    call_py.start()
+except Exception as e:
+    logging.error(f"Failed to start PyTgCalls: {e}")
+    raise
